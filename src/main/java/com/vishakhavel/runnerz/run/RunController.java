430e4292d1cfaf8e4f6124d91fd7ef78b341bda0
vichu <vichu@vishakhavels-MacBook-Air.local>
2024-07-13 02:29:39 -0700

    added get and getById

--- src/main/java/com/vishakhavel/runnerz/run/RunController.java
+++ src/main/java/com/vishakhavel/runnerz/run/RunController.java
@@ -1,21 +1,41 @@
 package com.vishakhavel.runnerz.run;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.util.*;
 
 @RestController
+//base mapping, all the uris inside will follow /api/runs
+@RequestMapping("/api/runs")
 public class RunController {
-    private  final RunRepository runRepository;
-//    dependency injection, since RunRepository is managed by spring, cos of the @Repository annotation !
-    public RunController(RunRepository runRepository){
+    private final RunRepository runRepository;
+
+    //    dependency injection, since RunRepository is managed by spring, cos of the @Repository annotation !
+    public RunController(RunRepository runRepository) {
 //        passing RunRepository in the args tells spring to inject it.
-            this.runRepository = runRepository;
+        this.runRepository = runRepository;
     }
-    @GetMapping("/api/runs")
-    List<Run> findAll(){
+
+    //    base URI
+    @GetMapping("")
+    List<Run> findAll() {
         return runRepository.findAll();
+    }
+
+    //     return the run  by id
+    @GetMapping("/{id}")
+    Run findById(@PathVariable Integer id) {
+        Optional run = runRepository.findById(id);
+        if (run.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "run not found");
+        } else {
+            return (Run) run.get();
+        }
     }
 
 }
--- src/main/java/com/vishakhavel/runnerz/run/RunRepository.java
+++ src/main/java/com/vishakhavel/runnerz/run/RunRepository.java
@@ -1,7 +1,9 @@
 package com.vishakhavel.runnerz.run;
+
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
 import java.util.*;
 
@@ -10,13 +12,17 @@ import java.util.*;
 public class RunRepository {
     private List<Run> runs = new ArrayList<>();
 
-    public List<Run> findAll(){
+    public List<Run> findAll() {
         return runs;
     }
 
     @PostConstruct
-    private void init(){
+    private void init() {
         runs.add(new Run(1, "run1", LocalDateTime.now(), LocalDateTime.now(), 3, Location.OUTDOOR));
         runs.add(new Run(2, "run2", LocalDateTime.now(), LocalDateTime.now(), 3, Location.INDOOR));
+    }
+
+    Optional findById(Integer id) {
+        return runs.stream().filter(run -> run.id() == id).findFirst();
     }
 }
